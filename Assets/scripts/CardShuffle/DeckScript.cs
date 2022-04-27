0858bd115dfe1a3b77aa27eb178937f2161d58ed
Hamster2903 <31683869+Hamster2903@users.noreply.github.com>
2022-04-27 11:45:17 +1000

    imported card deck framework
    imported card deck framework from other project and can now begin work on the project

---
+++ Assets/scripts/CardShuffle/DeckScript.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class DeckScript : MonoBehaviour
+{
+    //defines the card prefab and cardgroup so that the deck may be displayed visually
+    public GameObject cardPrefab;
+    public Transform cardGroup;
+    public Transform cardGroupFive;
+    //defines the list of cards as game objects
+    public List<GameObject> cards;
+    //defines all the required values of each card as string arrays
+    string[] face = new string[] { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+    string[] suit = new string[] { "Clubs", "Diamonds", "Hearts", "Spades" };
+    string[] iconSuit = new string[] { "♣", "♦", "♥", "♠" };
+    string[] iconFace = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+    //removes the first five cards in the deck and puts them in a seperate card group
+    public void DrawFive()
+    {
+        foreach (Transform item in cardGroupFive.transform)
+        {
+            Destroy(item.gameObject);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            cards[i].transform.SetParent(cardGroupFive);
+        }
+
+    }
+    //Generate() destroys and clears each game object in th list prior to generating the new list and card prefabs
+    public void Generate()
+    {
+        foreach (Transform item in cardGroup.transform)
+        {
+            Destroy(item.gameObject);
+        }
+        cards.Clear();
+        //each for loop cycles through the length of both face and suit to instantiate the values onto the card prefabs
+        for (int i = 0; i < face.Length; i++)
+        {
+            for (int f = 0; f < suit.Length; f++)
+            {
+                GameObject newCard = Instantiate(cardPrefab);
+                newCard.GetComponent<cardScript>().UpdateCard(face[i],suit[f],iconFace[i],iconSuit[f]);
+                cards.Add(newCard);
+                //sets the new cards to children of the cardgroup
+                newCard.transform.SetParent(cardGroup);
+                print(face[i] +" of "+suit[f]);
+            }
+        }
+    }
+    //shuffle removes the cards from the list, randomises there positions and re instates them in the list in their new positions
+    public void Shuffle()
+    {
+        for (int i = 0; i < 52; i++)
+        {
+            cards[i].transform.SetParent(transform);
+        }
+        for (int i = 0; i < 10000; i++)
+        {
+
+            int rnd = UnityEngine.Random.Range(0, 52);
+            int rnd1 = UnityEngine.Random.Range(0, 52);
+
+            GameObject temp = cards[rnd];
+            cards[rnd] = cards[rnd1];
+            cards[rnd1] = temp;
+        }
+        for (int i = 0; i < 52; i++)
+        {
+            cards[i].transform.SetParent(cardGroup);
+        }
+
+        //re add children to parent
+    }
+    
+}
+
+
---
+++ Assets/scripts/CardShuffle/cardScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class cardScript : MonoBehaviour
+{
+    // defines all the variables used on the card prefabs
+    public string face;
+    public string suit;
+    public string iconSuit;
+    public string iconFace;
+    public Text iconFaceTextLeft;
+    public Text iconSuitTextRight;
+    public Text iconSuitTextLeft;
+    public Text iconFaceTextRight;
+    //sets the card prefab with values defined in list on DeckScript
+    public void UpdateCard(string _face, string _suit, string _iconSuit, string _iconFace)
+    {
+        face = _face;
+        suit = _suit;      
+        iconFace = _iconFace;
+        iconSuit = _iconSuit;
+
+        iconFaceTextLeft.text = iconFace;
+        iconFaceTextRight.text = iconFace;
+        iconSuitTextLeft.text = iconSuit;
+        iconSuitTextRight.text = iconSuit;
+    }
+}
+
