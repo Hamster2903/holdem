2cb7269b526b1f968aa9139600f6c8a1cf246cb5
Hamster2903 <31683869+Hamster2903@users.noreply.github.com>
2022-06-09 21:00:46 +1000

    Revert "Update gameManager.cs"
    This reverts commit 3f61e70b817f010e1490fefd534d6fdcc6a200b4.

--- Assets/scripts/main game/gameManager.cs
+++ Assets/scripts/main game/gameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class gameManager : MonoBehaviour
 {
+       
     public DeckScript deckScript;
     public CardScript cardScript;
     public Button raiseButton;
